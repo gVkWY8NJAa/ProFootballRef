34dde6f6bbc1fad2d28020bccb969e06145e3e70
gVkWY8NJAa <gVkWY8NJAa-github@dops.33mail.com>
2018-04-16 20:18:08 -0500

    added travis, refactored unit tests

--- tests/test_GetPositionLinks.py
+++ tests/test_GetPositionLinks.py
@@ -1,52 +1,84 @@
-from ProFootballRef.LinkBuilder.GetPositionLinks import GetPositionLinks
+import pytest
+from ProFootballRef.LinkBuilder import GetPositionLinks
 
-receiving = GetPositionLinks('receiving').parse_links('2017')
-rushing = GetPositionLinks('rushing').parse_links('2017')
-passing = GetPositionLinks('passing').parse_links('2017')
-defense = GetPositionLinks('defense').parse_links('2017')
-kicking = GetPositionLinks('kicking').parse_links('2017')
 
-def test_receiving_type():
-    assert type(receiving) == list
+@pytest.fixture(scope="class")
+def receiving():
+    return GetPositionLinks.GetPositionLinks('receiving').parse_links('2017')
 
-def test_receiving_length():
-    assert len(receiving) > 10
 
-def test_receiving_value():
-    assert 'https://www.pro-football-reference.com/players/C/CookBr00.htm' in receiving
+@pytest.fixture(scope="class")
+def rushing():
+    return GetPositionLinks.GetPositionLinks('rushing').parse_links('2017')
 
-def test_rushing_type():
-    assert type(rushing) == list
 
-def test_rushing_length():
-    assert len(rushing) > 10
+@pytest.fixture(scope="class")
+def passing():
+    return GetPositionLinks.GetPositionLinks('passing').parse_links('2017')
 
-def test_rushing_value():
-    assert 'https://www.pro-football-reference.com/players/K/KamaAl00.htm' in rushing
 
-def test_passing_type():
-    assert type(passing) == list
+@pytest.fixture(scope="class")
+def defense():
+    return GetPositionLinks.GetPositionLinks('defense').parse_links('2017')
 
-def test_passing_len():
-    assert len(passing) > 10
 
-def test_passing_value():
-    assert 'https://www.pro-football-reference.com/players/N/NewtCa00.htm' in passing
+@pytest.fixture(scope="class")
+def kicking():
+    return GetPositionLinks.GetPositionLinks('kicking').parse_links('2017')
 
-def test_defense_type():
-    assert type(defense) == list
 
-def test_defense_len():
-    assert len(defense) > 10
+class TestReceiving:
 
-def test_defense_value():
-    assert 'https://www.pro-football-reference.com/players/O/OnwuPa00.htm' in defense
 
-def test_kicking_type():
-    assert type(kicking) == list
+    def test_receiving_type(self, receiving):
+        assert type(receiving) == list
 
-def test_kicking_len():
-    assert len(kicking) > 10
+    def test_receiving_length(self, receiving):
+        assert len(receiving) > 10
 
-def test_kicking_value():
-    assert 'https://www.pro-football-reference.com/players/Z/ZuerGr00.htm' in kicking
+    def test_receiving_value(self, receiving):
+        assert 'https://www.pro-football-reference.com/players/C/CookBr00.htm' in receiving
+
+
+class TestRushing:
+
+    def test_rushing_type(self, rushing):
+        assert type(rushing) == list
+
+    def test_rushing_length(self, rushing):
+        assert len(rushing) > 10
+
+    def test_rushing_value(self, rushing):
+        assert 'https://www.pro-football-reference.com/players/K/KamaAl00.htm' in rushing
+
+
+class TestPassing:
+    def test_passing_type(self, passing):
+        assert type(passing) == list
+
+    def test_passing_len(self, passing):
+        assert len(passing) > 10
+
+    def test_passing_value(self, passing):
+        assert 'https://www.pro-football-reference.com/players/N/NewtCa00.htm' in passing
+
+
+class TestDefense:
+    def test_defense_type(self, defense):
+        assert type(defense) == list
+
+    def test_defense_len(self, defense):
+        assert len(defense) > 10
+
+    def test_defense_value(self, defense):
+        assert 'https://www.pro-football-reference.com/players/O/OnwuPa00.htm' in defense
+
+class TestKicking:
+    def test_kicking_type(self, kicking):
+        assert type(kicking) == list
+
+    def test_kicking_len(self, kicking):
+        assert len(kicking) > 10
+
+    def test_kicking_value(self, kicking):
+        assert 'https://www.pro-football-reference.com/players/Z/ZuerGr00.htm' in kicking
